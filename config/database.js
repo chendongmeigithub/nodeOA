e4704a8e04ed8db4be450c137c3198702af659a7
chendongmei <1174411878@qq.com>
2017-07-03 18:06:54 +0800

    处理文件读取

--- config/database.js
+++ config/database.js
@@ -1,5 +1,18 @@
+var fs = require("fs");
 var mysql = require("mysql");
-var pool = mysql.createPool();
+var pool;
+
+fs.readFile('./config/config.json', 'utf-8', function (err, data) {
+    console.log(err, data);
+    if (err) {
+        console.log("error");
+    } else {
+        data = JSON.parse(data);
+        console.log(typeof data);
+        pool = mysql.createPool(data);
+    }
+});
+
 var query = function (sql, callback) {
     pool.getConnection(function (err, conn) {
         if (err) {
